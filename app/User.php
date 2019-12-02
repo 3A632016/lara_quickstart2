6a2cb14bf77c85a08b2c26049bb6b670e7b421f6
3A632016 <winnie771737@gmail.com>
2019-12-03 00:00:33 +0800

    建立user 和 tasks 的關聯

--- app/User.php
+++ app/User.php
@@ -10,6 +10,16 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    // 其他的 Eloquent 屬性...
+
+    /**
+     * 取得該使用者的所有任務。
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
